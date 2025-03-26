f013b9862ebf96bb1b857154f2a5d1c5af1a30b3
dakshpandey177@gmail.com <dakshpandey177@gmail.com>
2025-03-26 16:39:46 +0530

    corrected waveforms and testbench and everything lol

--- Chapter_2/2.9.1/gt_2.v
+++ Chapter_2/2.9.1/gt_2.v
@@ -1,12 +1,14 @@
 module gt_2(input wire [1:0] a, b,
 output wire y);
 
-wire p0, p1, p2;
+wire p0, p1, p2, p3, p4;
 
-assign y = p0|p1|p2;
+assign y = p0|p1|p2|p3|p4;
 
-assign p0 = ~b[1] & a[1];
-assign p1 = a[0] & (~b[1]) & (~b[0]);
-assign p2 = a[1] & a[0] & (~b[0]);
+assign p0 = ~b[0] & (~b[1]);
+assign p1 = a[0] & a[1];
+assign p2 = a[0] & (~b[1]);
+assign p3 = a[1] & (~b[0]);
+assign p4 = a[1] & (~b[1]);
 
 endmodule
